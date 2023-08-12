2ed07ec534b0e47b2dfc0c85479b73c01d920f0f
sajjadmoosavi90@gmail.com <sajjadmoosavi90@gmail.com>
2023-08-12 13:27:40 +0330

    before add bootstrap

---
+++ src/@types/userType.ts
@@ -0,0 +1,6 @@
+export type UserType = {
+  // email: string;
+  password: string;
+  username:string;
+  name:string
+};
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,25 +1,25 @@
 import { json } from "react-router-dom";
 import { api } from "../lib/api";
 import { createContext, useContext, useEffect, useState } from "react";
-
+import { UserType } from "../@types/userType";
 
 export const Users = () => {
-  const [users, setUsers] = useState<string[]>([]);
+  const [users, setUsers] = useState<UserType[]>([]);
   
 
   const GetUsers = async () => {
     try {
       
-      const res = await api.post<{}>("/users3");
+      const res = await api.get<{}>("/users2");
       //  const userslist: string[] = res.data.data;
-       let userslist: Array<string> = [JSON.stringify(res.data)];
+       //let userslist: Array<UserType> = (res["data"]);
 
-
-// console.log(JSON.stringify(res.data))
+console.log(res)
+      // console.log(JSON.stringify(res.data))
     
-     // console.log(userslist);
-     setUsers(prevNames => [...userslist]);
-console.log("users = "+ users)
+      //console.log(userslist);
+   //  setUsers(prevNames => [...userslist]);
+// console.log("users = "+ users)
       //localStorage.setItem("@Auth:access_token", res.data.token);
       
     } catch (err) {
